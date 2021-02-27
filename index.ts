2e168290391dc91bfbaedc1df92f5f00ed666382
Aldous Snow <aldous@untergrunddating.club>
2021-02-27 02:27:42 +0100

    Changed len to 100000

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-// @ts-nocheck
-const len = 10000;
-const fibos: Array<bigint> = Array.apply(null, new Array(len)).map(()=> 0n);
+declare var BigInt;
+const len = 100000;
+const fibos: Array<bigint> = Array.apply(null, new Array(len)).map(()=> BigInt(0));
 const fibo: (n: number, fibos: bigint[]) => bigint = (n, fibos) => {
   if ( n === 1 || n === 0 ) {
     return BigInt(n);
   }
-  if ( fibos[n - 1] === 0n ) {
+  if ( fibos[n - 1] === BigInt(0) ) {
     fibos[n - 1] = fibo(n - 1, fibos);
   }
-  if ( fibos[n - 2] === 0n ) {
+  if ( fibos[n - 2] === BigInt(0) ) {
     fibos[n - 2] = fibo(n - 1, fibos);
   }
   return BigInt(fibos[n - 1] + fibos[n - 2]);
@@ -20,4 +20,4 @@ for ( let i = 1; i < len; i++ ) {
   }
 }
 // You can use the numbers from array
-console.log(fibos[9999]);
+console.log(fibos[len - 1]);
